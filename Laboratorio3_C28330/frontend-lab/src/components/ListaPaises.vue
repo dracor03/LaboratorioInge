4d32b1ee5eb3b2962bcbebecf8558c972dae7f17
dracor03 <rvegassanabria@gmail.com>
2025-04-02 22:11:14 -0600

    Lab 3 completed

---
+++ Laboratorio3_C28330/frontend-lab/src/components/ListaPaises.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class = "container mt-5">
+        <h1 class = "display-4 text-center">Lista de países </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ListaPaises",
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
